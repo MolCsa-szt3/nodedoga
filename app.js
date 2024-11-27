58ad2c17f525720664fe689de46fadd9302c34e5
szt3molcsa <szt3molcsa@vasvari.org>
2024-11-27 10:02:23 +0100

    That oughta do it for the 3

--- app.js
+++ app.js
@@ -7,4 +7,13 @@ app.listen(PORT)
 app.get("/", (res,req)=>{
     req.send("Hi, There!")
 })
+app.get("/express", (res,req)=>{
+    req.send("Az Express egy minimalista webes keretrendszer, amely a Node.js-hez készült.")
+})
+app.get("/greeting", (res,req)=>{
+    req.send("Hello,  John Doe  ")
+})
+app.get("/nodejs", (res,req)=>{
+    req.send("A Node.js egy olyan szerveroldali JavaScript futtatókörnyezet, amely a V8 JavaScript motorra épül.")
+})
 
